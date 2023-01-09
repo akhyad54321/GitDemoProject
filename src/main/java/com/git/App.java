d135472e15b5aeb9eb8943291adb0733e173724e
Akhilesh Yadav <akhyad54321@gmail.com>
2023-01-09 19:02:42 +0530

    first commit

---
+++ src/main/java/com/git/App.java
@@ -0,0 +1,16 @@
+package com.git;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        int num = 100;
+        if(num%2==0) {
+        	System.out.println("Even");
+        }
+        else {
+        	System.out.println("Odd");
+        }
+    }
+}
